6fd6ab4cc8cfd97d3f049477dade9efaa5bf5115
wang90 <393210564@qq.com>
2020-10-15 14:38:20 +0800

    style: code style

--- puppeteer-BiBi/README.md
+++ puppeteer-BiBi/README.md
@@ -8,7 +8,7 @@ node index.js
 `````
 ### 参数
 `````
-start(number:number = 15) // 默认10 
+start( number: number = 15 ) // 默认10 
 `````
 ### 原理
 - 引入 puppeteer
@@ -21,7 +21,7 @@ const puppeteer = require("puppeteer");
 async function start() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.bilibili.com');
+    await page.goto( 'https://www.bilibili.com' );
     const cards = await page.evaluate(() => {
         // 获取网页内部内容
     };
@@ -30,31 +30,31 @@ async function start() {
 - 根据标签寻找内容
 `````
 const cs = document.querySelectorAll('.video-card-common');
-const cards =  [...cs].map(card => {
-    const title = getText(card,'.ex-title') ||  getText(card,'.title');
-    const up = getText(card,'.ex-up') || getText(card,'text');
-    return { title, up};
+const cards =  [ ...cs ].map( card => {
+    const title = getText( card, '.ex-title' ) ||  getText( card, '.title' );
+    const up = getText( card, '.ex-up' ) || getText(card,'text');
+    return { title, up };
 });
-function getText(card,className) {
-    if (card.querySelector(className)) {
-        return card.querySelector(className).innerText.trim();
+function getText( card, className ) {
+    if (card.querySelector( className )) {
+        return card.querySelector( className ).innerText.trim();
     }
     return null;
 }
 `````
 - 滚动加载
 ``````
-function next(n , callback) {
-    if (n > 0 ) {
-        window.scrollBy(0,window.innerHeight);
-    }else{
+function next( n , callback ) {
+    if ( n > 0 ) {
+        window.scrollBy( 0, window.innerHeight );
+    } else {
         callback();
         return ;
     }
 
     setTimeout(() => {
-        next( n - 1 , callback);
-    },1000);
+        next( n - 1 , callback );
+    }, 1000);
 }
 ``````
 - 启用
--- puppeteer-BiBi/index.js
+++ puppeteer-BiBi/index.js
@@ -3,16 +3,16 @@
 
 const puppeteer = require("puppeteer");
 
-async function start(number = 10) {
+async function start( number = 10 ) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.bilibili.com');
+    await page.goto( 'https://www.bilibili.com' );
 
     const cards = await page.evaluate(() => {
         function next(n , callback) {
-            if (n > 0 ) {
-                window.scrollBy(0,window.innerHeight);
-            }else{
+            if ( n > 0 ) {
+                window.scrollBy( 0, window.innerHeight );
+            } else {
                 callback();
                 return ;
             }
@@ -21,31 +21,31 @@ async function start(number = 10) {
                 next( n - 1 , callback);
             },1000);
         }
-        function getText(card,className) {
-            if (card.querySelector(className)) {
-                return card.querySelector(className).innerText.trim();
+        function getText( card, className ) {
+            if ( card.querySelector( className )) {
+                return card.querySelector( className ).innerText.trim();
             }
             return null;
         }
-        return new Promise((reslove,rejected)=> {
+        return new Promise(( reslove, rejected ) => {
             next( number , () => {
                 const cs = document.querySelectorAll('.video-card-common');
-                const cards =  [...cs].map(card => {
-                    const title = getText(card,'.ex-title') ||  getText(card,'.title');
-                    const up = getText(card,'.ex-up') || getText(card,'text') ;                                                                             
+                const cards =  [ ...cs ].map( card => {
+                    const title = getText( card, '.ex-title' ) ||  getText(card,'.title');
+                    const up = getText( card, '.ex-up' ) || getText(card,'text') ;                                                                             
                     return {
                         title,
                         up,
                     };
                 });
-                reslove(cards);
+                reslove( cards );
             });
         });    
-    }).then(res=>{
-        console.log("is list");
-        console.log(res.length);
-    }).catch(err=>{
-        console.log(err);
+    }).then(res => {
+        console.log( "is list" );
+        console.log( res.length );
+    }).catch( err => {
+        console.log( err );
     });
 }
 start();
